7bc5db7de713c5902f0a74df68d43ff1dc8027bb
Matt Sell <mwsell@gmail.com>
2017-06-04 15:23:19 -0500

    implement host

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	var isHost bool
+	flag.BoolVar(&isHost, "listen", false, "Listens on the specified ip address")
+	flag.Parse()
+
+	if isHost {
+		connIP := os.Args[2]
+		runHost(connIP)
+	} else {
+		connIP := os.Args[1]
+		runGuest(connIP)
+	}
+}
+
+const port = "8080"
+
+func runHost(ip string) {
+	ipAndPort := ip + ":" + port
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+
+	if listenErr != nil {
+		log.Fatal("Error ", listenErr)
+	}
+
+	conn, acceptErr := listener.Accept()
+	if acceptErr != nil {
+		log.Fatal("Error: ", acceptErr)
+	}
+
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error ", readErr)
+	}
+	fmt.Println("Message recieved: ", message)
+}
+
+func runGuest(ip string) {
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+To send messags
+
+netcat - general purpose networking tool for tcp/udp
+
+nc localhost 8080
